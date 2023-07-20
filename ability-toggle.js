60b9b50892c79985e303866428ebdc5d1b33fdcf
Stratos Neiros <nstratos@gmail.com>
2023-07-20 16:48:58 +0300

    First commit

---
+++ ability-toggle.js
@@ -0,0 +1,148 @@
+import stylesheet from "./ability-toggle.css" assert { type: "css" }
+
+const template = document.createElement("template")
+template.innerHTML = `
+  <div>
+    <style>
+      .ability-image {filter: grayscale(1);}
+    </style>
+    <slot class="ability-image" name="ability-image"></slot>
+    <slot name="ability-text"></slot>
+  </div>
+`
+const templateContent = template.content;
+
+class AbilityToggle extends HTMLElement {
+  id = "0";
+  enabled = false;
+  obtained = false;
+  unlockedBy = new Map();
+  children = new Map();
+  #image = null;
+  
+  static get observedAttributes() {
+    return ["enabled", "obtained", "unlockedBy", "children"];
+  }
+  constructor() {
+    super();
+
+    let shadowRoot = this.attachShadow({ mode: "open" });
+    shadowRoot.adoptedStyleSheets = [stylesheet];
+
+    if (this.hasAttribute("id")) {
+      this.id = this.getAttribute("id");
+    }
+    if (this.hasAttribute("enabled")) {
+      this.enabled = true;
+    }
+    if (this.hasAttribute("unlocked-by")) {
+      this.getAttribute("unlocked-by").split(",").forEach(element => this.unlockedBy.set(element, false));
+    }
+    if (this.hasAttribute("children")) {
+      this.getAttribute("children").split(",").forEach(element => this.children.set(element, false));
+    }
+    console.log("id=", this.id, "->", this.unlockedBy);
+    
+    this.#image = document.createElement("img");
+    this.#image.className = "ability-toggle-img"
+    this.#image.src = "img/default.png";
+    this.#image.alt = "Unknown ability";
+    if (this.hasAttribute("img")) {
+      this.#image.src = this.getAttribute("img");
+      this.#image.alt = this.hasAttribute("alt") ? this.getAttribute("alt") : "";
+    }
+    shadowRoot.appendChild(this.#image);
+
+    
+    
+    //shadowRoot.appendChild(template.content.cloneNode(true))
+    
+  }
+
+  obtainedToggle() {
+    if (this.enabled === false) {
+      return;
+    }
+    this.obtained = true
+    this.#image.style.filter = "grayscale(0) brightness(1)";
+    this.dispatchEvent(
+      new CustomEvent('ability-obtained', {
+        detail: {
+          id: this.id,
+          obtained: this.obtained,
+        },
+        bubbles: true,
+      }),
+    );
+  }
+
+  remove() {
+    if (this.enabled === false) {
+      return;
+    }
+    this.obtained = false;
+    this.#image.style.filter = "grayscale(1) brightness(0.8)";
+    this.dispatchEvent(
+      new CustomEvent('ability-removed', {
+        detail: {
+          id: this.id,
+          obtained: this.obtained,
+        },
+        bubbles: true,
+      }),
+    );
+  }
+
+  connectedCallback () {
+    this.addEventListener("click", function(event) {
+      event.preventDefault();
+      this.obtainedToggle();
+    });
+    this.addEventListener("contextmenu", function(event) {
+      event.preventDefault();
+      this.remove();
+    });
+    this.addEventListener("obtained-abilities", this.handleObtainedAbilities);
+  }
+
+  handleObtainedAbilities(event) {
+    console.log("id=", this.id, this.unlockedBy);
+    console.log("obtained abilities = ", event.detail.obtainedAbilities);
+    for (let key of this.unlockedBy.keys()) {
+      this.unlockedBy.set(key, event.detail.obtainedAbilities.get(key) ?? false)
+    }
+    for (let key of this.children.keys()) {
+      this.children.set(key, event.detail.obtainedAbilities.get(key) ?? false)
+    }
+    console.log("children=", this.children);
+    if (this.unlockedBy.size == 0) {
+      console.log("size =0")
+      return
+    }
+    const values = [...this.unlockedBy.values()]
+    console.log("id=", this.id, this.unlockedBy);
+    if (!values.includes(false)) {
+      this.enabled = true;
+    } else {
+      this.enabled = false;
+    }
+    console.log("id=", this.id, "enabled=", this.enabled);
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    switch (name) {
+      case 'unlockedBy':
+        console.log('++++ unlockedBy changed.');
+        break;
+      case 'children':
+        console.log('++++ children changed.');
+        break;
+    }
+    console.log(name, '++++ changed.');
+  }
+}
+
+customElements.define(
+  "ability-toggle",
+  AbilityToggle
+)
---
+++ ability-tree.js
@@ -0,0 +1,59 @@
+import stylesheet from "./ability-tree.css" assert { type: "css" }
+
+class AbilityTree extends HTMLElement {
+  obtainedAbilities = new Map();
+  constructor() {
+    super();
+    
+    let shadowRoot = this.attachShadow({ mode: "open" });
+    shadowRoot.adoptedStyleSheets = [stylesheet];
+    const wrapper = document.createElement("slot");
+    shadowRoot.appendChild(wrapper);
+
+  }
+  connectedCallback() {
+    this.addEventListener("ability-obtained", this.handleAbilityObtained)
+    this.addEventListener("ability-removed", this.handleAbilityRemoved)
+    // let bb = this.querySelectorAll("ability-toggle");
+    let bb = this.children;
+    for (let ability of bb) {
+      console.log("tree-> ", ability.id, ability.obtained);
+      console.log("tree-> ", ability.getAttribute("id"), ability.getAttribute("obtained"));
+      console.log(ability);
+      this.obtainedAbilities.set(ability.getAttribute("id"), ability.getAttribute("obtained"));
+    }
+    
+  }
+
+  handleAbilityObtained(event) {
+    this.obtainedAbilities.set(event.detail.id, true);
+    for (let ability of this.childNodes) {
+      // this.unlockedBy.set(key, event.detail.obtainedAbilities.get(key))
+      this.publishObtainedAbilities(ability);
+    }
+    // this.childNodes.forEach(this.publishObtainedAbilities);
+    
+    // console.log(this.obtainedAbilities);
+  }
+
+  handleAbilityRemoved(event) {
+    this.obtainedAbilities.set(event.detail.id, false);
+    for (let ability of this.childNodes) {
+      this.publishObtainedAbilities(ability);
+    }
+  }
+
+  publishObtainedAbilities(target) {
+    const event = new CustomEvent("obtained-abilities", {
+      detail: {
+        obtainedAbilities: this.obtainedAbilities,
+      },
+    });
+    target.dispatchEvent(event);
+  }
+}
+
+customElements.define(
+  "ability-tree",
+  AbilityTree
+)
