7a04720a74481635d03f4e8d9caa15ab0175c894
Stratos Neiros <nstratos@gmail.com>
2025-01-31 19:27:21 +0200

    Add comment to indicate version.js will be overwritten during deployment

--- version.js
+++ version.js
@@ -1,3 +1,4 @@
+// This file will be overwritten during deployment.
 export const APP_VERSION = 'dev';
 export const APP_URL = "https://nstratos.github.io/stoneshard-talent-calculator";
 export const REPO_URL = "https://github.com/nstratos/stoneshard-talent-calculator";
