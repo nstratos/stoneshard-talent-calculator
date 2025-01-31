da207193bd676ddc245ebd0abd17b1b2acb50089
Stratos Neiros <nstratos@gmail.com>
2025-01-31 20:39:56 +0200

    Fix logo link leading to 404 (#4)

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
 // This file will be overwritten during deployment.
 export const APP_VERSION = 'dev';
-export const APP_URL = "https://nstratos.github.io/stoneshard-talent-calculator";
+export const APP_URL = "/";
 export const REPO_URL = "https://github.com/nstratos/stoneshard-talent-calculator";
