3aece0d6e290de8f617c3362ad37251b484c87c6
Stratos Neiros <nstratos@gmail.com>
2025-02-27 01:10:06 +0200

    Tooltips support (#45)
    - Support for showing tooltips on mouse over
    - Show tooltips on long press for touch devices
    - Tooltips fade-in effect
    - Support for tooltip formulas

---
+++ components/stat-formula/stat-formula.js
@@ -0,0 +1,46 @@
+import Character from './character.js';
+
+class StatFormula extends HTMLElement {
+  #character = null;
+  #formula = '';
+  #eval = '';
+  #result = '';
+  constructor() {
+    super();
+
+    this.#character = new Character();
+
+    let shadowRoot = this.attachShadow({ mode: 'open' });
+    const slot = document.createElement('slot');
+    shadowRoot.appendChild(slot);
+  }
+
+  connectedCallback () {
+    this.#formula = this.innerHTML;
+    console.log();
+    this.#eval = this.#replaceStats(this.#formula);
+    this.#result = eval?.(`"use strict";(${this.#eval})`);
+    this.#result = (this.#result <= 0 ? '' : '+') + this.#result; // Add plus sign in case of positive result.
+    console.log(`${this.#formula} = ${this.#result}`);
+    this.innerHTML = this.#result;
+  }
+
+  #replaceStats(formula) {
+    formula = formula.replaceAll('STR', this.#character.strength);
+    formula = formula.replaceAll('AGL', this.#character.agility);
+    formula = formula.replaceAll('PRC', this.#character.perception);
+    formula = formula.replaceAll('VIT', this.#character.vitality);
+    formula = formula.replaceAll('WIL', this.#character.willpower);
+    return formula;
+  }
+
+  showFormula() {
+    this.innerHTML = this.#formula;
+  }
+
+  hideFormula() {
+    this.innerHTML = this.#result;
+  }
+}
+
+customElements.define('stat-formula', StatFormula)
