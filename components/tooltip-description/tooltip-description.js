71f26dcefe70a3389cb3445e26725dd0589d7c4d
Stratos Neiros <nstratos@gmail.com>
2025-03-02 06:49:03 +0200

    Add electromancy tier 4 tooltips

--- components/tooltip-description/tooltip-description.js
+++ components/tooltip-description/tooltip-description.js
@@ -167,6 +167,11 @@ class TooltipDescription extends HTMLElement {
       const formulas = input[0];
       description = input[1];
       formulaMap = this.#parseFormulas(formulas);
+      // Sort so that longer formulas appear first to avoid the case where a formula like HP_Limit
+      // would be applied before Max_HP_Limit and replace part of the longer formula.
+      formulaMap = Object.fromEntries(
+        Object.entries(formulaMap).sort(([,a],[,b]) => b.length - a.length)
+      );
     }
     const data = description.split(';');
     const tooltip = {
