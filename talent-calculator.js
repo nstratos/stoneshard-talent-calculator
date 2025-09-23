97953022942c651291422a135c74fd3aea260295
Stratos Neiros <nstratos@gmail.com>
2025-09-23 20:02:29 +0300

    Show stat increase order visually

--- talent-calculator.js
+++ talent-calculator.js
@@ -225,6 +225,7 @@ class TalentCalculator extends HTMLElement {
       });
 
       this.#updateStatsDisplay();
+      this.#updateStatIncreaseDisplay();
   }
 
   #updateStatPointsDisplay() {
@@ -235,6 +236,11 @@ class TalentCalculator extends HTMLElement {
       });
     } else {
       this.querySelectorAll(".plus-button:not(#level-up-button)").forEach(statButton => {
+        if (this.#character.strength === 30 && statButton.id === 'up-str-button') return;
+        if (this.#character.agility === 30 && statButton.id === 'up-agi-button') return;
+        if (this.#character.perception === 30 && statButton.id === 'up-per-button') return;
+        if (this.#character.vitality === 30 && statButton.id === 'up-vit-button') return;
+        if (this.#character.willpower === 30 && statButton.id === 'up-wil-button') return;
         statButton.style.visibility = 'visible';
       });
     }
@@ -250,6 +256,15 @@ class TalentCalculator extends HTMLElement {
     this.#perDisplay.textContent = this.#character.perception;
     this.#vitDisplay.textContent = this.#character.vitality;
     this.#wilDisplay.textContent = this.#character.willpower;
+  }
+
+  #updateStatIncreaseDisplay() {
+    const makeStatIncreaseTemplate = (level, stat) => {
+      return `<p class="stat-increase"><span>Lvl <span class="stat-increase-level">${level}</span></span> <span class="stat-increase-${stat.toLowerCase()}">${stat.toUpperCase()} <span class="up">⬆</span></span></p>`;
+    }
+    const statIncreaseDisplay = this.querySelector('.stat-increase-order');
+    statIncreaseDisplay.innerHTML = this.#statStack.map(entry => makeStatIncreaseTemplate(entry.level, entry.stat))
+      .join('');
   }
 
   #buttonClickWithAnalytics(button, callback = () => {}) {
@@ -313,6 +328,7 @@ class TalentCalculator extends HTMLElement {
       this.#updateLevelDisplay();
       this.#updateStatPointsDisplay();
       this.#updateStatsDisplay();
+      this.#updateStatIncreaseDisplay();
     }
   }
 
