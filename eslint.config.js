1d6cefc2f72c37418d5940fae1dd21d45f869274
Stratos Neiros <nstratos@gmail.com>
2025-12-28 14:42:48 +0200

    Add ESLint and Prettier tooling via Docker

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    files: ["**/*.js"],
+    ignores: ["**/dist/**", "**/vendor/**"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module"
+    },
+    rules: {
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      "no-undef": "error",
+      "eqeqeq": ["error", "always"],
+      "no-implicit-coercion": ["warn", { "boolean": true }]
+    }
+  }
+];
